5bc7cd7bdd5147a49b27414a1bea2aa1a05dca06
Axel Herrera <73140275+Axelh98@users.noreply.github.com>
2022-03-27 18:44:04 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,129 @@
+// VARIABLES
+const formulario = document.querySelector('#formulario');
+const listaTweets = document.querySelector('#lista-tweets');
+let tweets = [];
+
+
+
+// EVENT LIsTENERS
+eventListeners();
+
+function eventListeners() {
+    
+    formulario.addEventListener('submit', agregarTweet);
+
+    // documento listo
+    document.addEventListener('DOMContentLoaded', () => {
+        tweets = JSON.parse( localStorage.getItem('tweets')) || [];
+
+        console.log(tweets);
+
+        creatHTML();
+    })
+}
+
+
+
+// FUNCIONES
+function agregarTweet(e) {
+    e.preventDefault();
+
+    // Donde se escribe
+    const tweet = document.querySelector('#tweet').value;
+
+    if (tweet === '') {
+        mostrarError('Un mensaje no puede ir vacio')
+        return;
+    }
+
+    const tweetobj = {
+        id: Date.now(),
+        tweet: tweet
+    }
+
+    // AÑADIR LOS TWEETS 
+    tweets = [...tweets, tweetobj];
+    console.log(tweets);
+
+    // Agregar HTML
+    creatHTML();
+
+    // REINICIAR FORMULARIO
+    formulario.reset();
+}
+
+// Mostrar mensaje
+
+function mostrarError(error){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // insertarlo en el HTML
+
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError)
+
+    setTimeout( () => {
+        mensajeError.remove();
+    }, 2000);
+}
+
+// Listaod de TWEETS
+
+function creatHTML() {
+
+    limpiarHTML();
+    
+    if (tweets.length > 0) {
+        tweets.forEach( tweet => {
+            // HTML
+
+            // BOTON ELIMINAR
+            const btnEliminar = document.createElement('a');
+            btnEliminar.classList.add('borrar-tweet');
+            btnEliminar.innerText = "X"
+
+
+            const li = document.createElement('li')
+
+            // AÑADIR EL TEXTO
+            li.innerText = tweet.tweet;
+
+            // ASIGNAR EL BOTON
+            li.appendChild(btnEliminar);
+
+            // AÑADIR FUNCION ELIMINAR
+            btnEliminar.onclick = () => {
+                borrarTweet(tweet.id);
+            }
+
+            listaTweets.appendChild(li);
+        });
+    }
+
+    sincronizarStorage();
+}
+
+// AGREGAR al LOCAL STOGARE
+function sincronizarStorage() {
+    localStorage.setItem('tweets', JSON.stringify(tweets));
+}
+
+// 
+function borrarTweet(id) {
+    tweets = tweets.filter( tweet => tweet.id !== id);
+    creatHTML();
+}
+
+// LIMPIAR HTML
+
+function limpiarHTML() {
+
+    while ( listaTweets.firstChild){
+        listaTweets.removeChild(listaTweets.firstChild)
+    }
+}
+
+
+
